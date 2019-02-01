4e9cea9fa07327bfd64179efb4582498050b075b
Siarhei Radkavets <Siarhei_Radkavets@epam.com>
2019-02-01 14:10:47 +0300

    add side menu component

---
+++ src/component/SideMenu/SideMenu.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+class SideMenu extends React.Component {
+  state = {
+    anchorEl: null,
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+
+    return (
+      <div>
+        <IconButton
+          color="inherit"
+          aria-owns={anchorEl ? 'simple-menu' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+        >
+          <MenuItem onClick={this.handleClose}>Catalog</MenuItem>
+          <MenuItem onClick={this.handleClose}>Admin</MenuItem>
+          <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default SideMenu;
--- src/component/ToolBar/ToolBar.js
+++ src/component/ToolBar/ToolBar.js
@@ -5,8 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import SideMenu from '../SideMenu/SideMenu'
 
 const styles = {
   root: {
@@ -23,13 +22,12 @@ const styles = {
 
 const ToolBar = (props) => {
   const { classes } = props;
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
+          <SideMenu className={classes.menuButton}/>
           <Typography variant="h6" color="inherit" className={classes.grow}>
             News
           </Typography>
