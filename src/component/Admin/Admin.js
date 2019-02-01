21406782ff1cac92fa61d13902eeece3a53b758c
Siarhei Radkavets <Siarhei_Radkavets@epam.com>
2019-02-01 16:41:40 +0300

    add products on admin

--- src/component/Admin/Admin.js
+++ src/component/Admin/Admin.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import {connect} from "react-redux";
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 800,
+    backgroundColor: theme.palette.background.paper,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  },
+  inline: {
+    display: 'inline',
+  },
+});
 
 const Admin = (props) => {
-  return <p>This is admin page</p>
+  const { classes } = props;
+  return (
+    <List className={classes.root}>
+      {props.products.map((product, i) => (
+          <ListItem alignItems="flex-start" key={i} divider>
+            <ListItemText
+              primary={product.title}
+              secondary={
+                <>
+                  <Typography component="span" className={classes.inline} color="textPrimary">
+                    {`${product.price}$ `}
+                  </Typography>
+                  {product.description}
+                </>
+              }
+            />
+            <Button variant="contained" color="primary">
+              Удалить
+            </Button>
+          </ListItem>
+        )
+      )}
+    </List>
+  )
 }
 
-export default Admin;
+const mapStateToProps = state => {
+  return {
+    products: state.products
+  };
+};
+
+export default connect(mapStateToProps)(withStyles(styles)(Admin));
